9510bcceebabb907865dce6792ffecd2db7bdd97
rupeshtiwari <roopkt@gmail.com>
2021-01-15 15:04:18 -0500

    fix: fixing ghpage routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { UiLoginComponent } from 'projects/ui-login/src/public-api';
 import { CoursesComponent } from './coures/courses/courses.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'courses' },
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+  { path: 'fsms-courses-angular-app', redirectTo: 'courses', pathMatch: 'full' },
   { path: 'courses', component: CoursesComponent },
   { path: 'login', component: UiLoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
