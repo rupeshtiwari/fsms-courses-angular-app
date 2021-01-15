1892c102f3a4dd2b8d0d0ea8ddb324e4e9f542a4
rupeshtiwari <roopkt@gmail.com>
2021-01-15 15:02:08 -0500

    build: added deploy feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { UiLoginComponent } from 'projects/ui-login/src/public-api';
 import { CoursesComponent } from './coures/courses/courses.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+  { path: '', redirectTo: 'courses' },
   { path: 'courses', component: CoursesComponent },
   { path: 'login', component: UiLoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
